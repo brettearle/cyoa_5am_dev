740686992e7be3d6a14771a02ccb1038757575ef
brett <brettearle@gmail.com>
2025-06-15 07:29:02 +0930

    feat(json parser) fragile string value extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn extract_value(json: &str, key: &str) -> String {
+    let pattern = format!(r#""{}": "#, key);
+    let start = json.find(&pattern).unwrap() + pattern.len();
+    let value_start = start + 1;
+    let value_end = json[value_start..].find('"').unwrap() + value_start;
+    json[value_start..value_end].to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_value_happy_path() {
+        let json = r#"
+        {
+            "text": "Follow a distant howl",
+            "next": "2a"
+        }"#;
+
+        let text = extract_value(json, "text");
+        let next = extract_value(json, "next");
+        let expected_text = "Follow a distant howl";
+        let expected_next = "2a";
+        assert_eq!(text, expected_text);
+        assert_eq!(next, expected_next)
+    }
+}
